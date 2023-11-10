cbb5f4c395876ce657b01e4399fd5198abb2ec1e
Maxim Kucheriaviy <maxim-k@i.ua>
2023-11-10 14:25:53 +0200

    server cout of page added

--- app/contollsers/getAllNews.js
+++ app/contollsers/getAllNews.js
@@ -2,9 +2,10 @@ const { News } = require("../models");
 
 module.exports = async (req, res) => {
   const { old = false, page = 1, perPage = 4 } = req.query;
+  const pagesCount = Math.floor((await News.find()).length / perPage) + 1;
   const news = await News.find()
     .sort({ date: old === "true" ? 1 : -1 })
     .skip((page - 1) * perPage)
     .limit(perPage);
-  res.status(201).json(news);
+  res.status(201).json({ news, pagesCount });
 };
