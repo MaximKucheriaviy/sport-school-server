b3112bb5a5b2c7c036ec649ef7ada2d0120329ab
Maxim Kucheriaviy <maxim-k@i.ua>
2023-11-10 09:18:09 +0200

    news pagination fix

--- app/contollsers/getAllNews.js
+++ app/contollsers/getAllNews.js
@@ -1,6 +1,10 @@
 const { News } = require("../models");
 
 module.exports = async (req, res) => {
-  const news = await News.find();
+  const { old = false, page = 1, perPage = 4 } = req.query;
+  const news = await News.find()
+    .sort({ date: old === "true" ? 1 : -1 })
+    .skip((page - 1) * perPage)
+    .limit(perPage);
   res.status(201).json(news);
 };
