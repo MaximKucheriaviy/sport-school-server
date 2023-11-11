11a1736de2e1b0be916561546a64c40c329cd1ba
MaximKucheriaviy <96634366+MaximKucheriaviy@users.noreply.github.com>
2023-11-11 08:20:05 +0200

    keyword fix

--- app/contollsers/getAllNews.js
+++ app/contollsers/getAllNews.js
@@ -1,9 +1,13 @@
 const { News } = require("../models");
 
 module.exports = async (req, res) => {
-  const { old = false, page = 1, perPage = 4 } = req.query;
-  const pagesCount = Math.floor((await News.find()).length / perPage) + 1;
-  const news = await News.find()
+  const { keyword = "", old = false, page = 1, perPage = 4 } = req.query;
+  const pagesCount =
+    Math.floor(
+      (await News.find({ title: { $regex: new RegExp(keyword, "i") } }))
+        .length / perPage
+    ) + 1;
+  const news = await News.find({ title: { $regex: new RegExp(keyword, "i") } })
     .sort({ date: old === "true" ? 1 : -1 })
     .skip((page - 1) * perPage)
     .limit(perPage);
